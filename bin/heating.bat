20e0c98e9cd3079343d7ca7895d6a1cead317131
leewalkergm <leewalkergm@gmail.com>
2015-12-10 23:32:12 +0000

    Worked on the heating stuff

--- bin/heating.bat
+++ bin/heating.bat
@@ -1,5 +1,8 @@
 echo off
 
 set temp=%1
+set nestUname=%2
+set nestPword=%3
 
 node ../src/heating.js
+pause
--- src/heating.js
+++ src/heating.js
@@ -1,1 +1,32 @@
-console.log("Heating to: " + process.env.temp);
+var nest = require('unofficial-nest-api'),
+    util = require('util');
+
+var username    = process.env.nestUname,
+    password    = process.env.nestPword,
+    temp        = process.env.temp;
+
+
+nest.login(username, password, function (err, data) {
+    if (err) {
+        console.log(err.message);
+        process.exit(1);
+        return;
+    }
+
+    console.log('Logged in.');
+
+    nest.fetchStatus(function (data) {
+        for (var deviceId in data.device) {
+            if (data.device.hasOwnProperty(deviceId)) {
+                var device = data.shared[deviceId];
+                console.log(util.format("%s [%s], Current temperature = %d C target=%d C",
+                    device.name, deviceId,
+                    device.current_temperature,
+                    device.target_temperature));
+            }
+        }
+        var ids = nest.getDeviceIds();
+        nest.setTargetTemperatureType(ids[0], 'heat');
+        nest.setTemperature(temp);
+    });
+});
